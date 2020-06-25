972a756113c2c71d6e2a127100c7908b5db0072b
liu <bruce.lhf@qq.com>
2020-06-26 02:32:35 +0800

    小🐦伏特加来👌🏻高 雷霆噶巴 整点儿🇷🇺大🐮子 哈拉少～ ZBC👍

--- src/components/digit-roll/index.vue
+++ src/components/digit-roll/index.vue
@@ -42,7 +42,7 @@ let DIGIT_FORMAT = "(,ddd).dd";
 let FORMAT_PARSER = /^\(?([^)]*)\)?(?:(.)(d+))?$/;
 
 export default {
-  name: "digit-roll",
+  name: "number-roll",
   model: {
     prop: "value",
     event: "change",
